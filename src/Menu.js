f7f79ac9940f9b4dc8a8d381e3ce0411cca0ca8e
Jean Reynoso <jreynoso.mena@gmail.com>
2022-10-27 15:33:05 -0500

    feat: se agrego menu

---
+++ src/Menu.js
@@ -0,0 +1,23 @@
+import './Menu.css';
+
+function Menu() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <a href="/">Inicio</a>
+          </li>
+          <li>
+            <a href="">Nosotros</a>
+          </li>
+          <li>
+            <a href="">Contacto</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default Menu;
