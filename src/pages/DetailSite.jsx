57889a22dab4bc279697615321e34d1de33c89e1
Jean Reynoso <jreynoso.mena@gmail.com>
2022-11-30 20:31:13 -0500

    feat: se cambio vista

--- src/pages/DetailSite.jsx
+++ src/pages/DetailSite.jsx
@@ -1,30 +1,62 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-const Route = ({}) => {
-  return(
-    <div style={{ width: '50%', margin: '20px', border: '1px solid gray', borderRadius: '15px'}}>
-      <p>Ruta: Nombre</p>
-      <p>Description</p>
-      <p>Inicio: </p>
-      <p>Fin: </p>
+const Route = ({ name, description, places }) => {
+  return (
+    <div
+      style={{
+        width: "50%",
+        margin: "20px",
+      }}
+    >
+      <p>
+        <b>Ruta:</b> {name}
+      </p>
+      <p>
+        <b>Description:</b> {description}
+      </p>
+      <p>
+        <b>Lugares:</b> {places.map((e) => e.name).join(",")}
+      </p>
     </div>
-  )
-}
+  );
+};
 const DetailSite = () => {
-  return(
-    <div style={{padding: '0 15%', display: 'block'}}>
-      <img style={{ objectFit: 'fill'}} src="https://sorprendete.pe/wp-content/uploads/2021/11/NOTA-MANCORA-02.jpg" width={'100%'} height={"450px"}/>
-      <div style={{paddingLeft: '45px'}}>
-        <h1>Mancora</h1>
-        <p>Máncora es un balneario y localidad peruana capital del distrito homónimo ubicado en la provincia de Talara, en el departamento de Piura.</p>
-        <div style={{ display: 'flex', flexWrap: 'wrap'}}>
-          <Route />
-          <Route />
-          <Route />
+  const [routes, setRoutes] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://us-central1-nodevs.cloudfunctions.net/fastifyFunction/routes"
+      )
+      .then(function ({ data: { payload } }) {
+        setRoutes(payload);
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+      .finally(function () {
+        // always executed
+      });
+  }, []);
+
+  return (
+    <div style={{ padding: "0 15%", display: "block" }}>
+      <img
+        style={{ objectFit: "fill" }}
+        src="https://travel1tours.com/wp-content/uploads/2019/09/Lugares-turisticos-de-Peru.jpg"
+        width={"100%"}
+        height={"450px"}
+      />
+      <div style={{ paddingLeft: "45px" }}>
+        <div style={{ display: "flex", flexWrap: "wrap" }}>
+          {routes.map((el) => (
+            <Route {...el} />
+          ))}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default DetailSite
+export default DetailSite;
